1bd58075e999da56f111fcea075fc9ff15301d6a
esscova <wsantos08@hotmail.com>
2026-08-26 22:11:10 -0300

    feat(Banco de Dados): adiciona arquivo sql para exercicio 12 em fundamentos

---
+++ Banco de Dados/Banco de Dados e Linguagem SQL com PostgreSQL/Parte 1 - fundamentos/12_exercicios-funcoes-agregadas.sql
@@ -0,0 +1,122 @@
+/*
+    Exercícios sobre funções agregadas (AVG, COUNT, SUM, MAX, MIN) e agrupamento (GROUP BY, HAVING)
+*/
+
+-- 12.1. A média dos valores de vendas dos vendedores que venderam mais que R$ 200,00.
+
+
+-- 12.2. Os vendedores que venderam mais que R$ 1.500,00.
+
+
+-- 12.3. O somatório das vendas de cada vendedor.
+
+
+-- 12.4. A quantidade de municípios.
+
+
+-- 12.5. A quantidade de municípios que são do Paraná ou de Santa Catarina.
+
+
+-- 12.6. A quantidade de municípios por estado.
+
+
+-- 12.7. A quantidade de clientes que informaram o logradouro.
+
+
+-- 12.8. A quantidade de clientes por município.
+
+
+-- 12.9. A quantidade de fornecedores.
+
+
+-- 12.10. A quantidade de produtos por fornecedor.
+
+
+-- 12.11. A média de preços dos produtos do fornecedor Cap. Computadores.
+
+
+-- 12.12. O somatório dos preços de todos os produtos.
+
+
+-- 12.13. O nome do produto e o preço somente do produto mais caro.
+
+
+-- 12.14. O nome do produto e o preço somente do produto mais barato.
+
+
+-- 12.15. A média de preço de todos os produtos.
+
+
+-- 12.16. A quantidade de transportadoras.
+
+
+-- 12.17. A média do valor de todos os pedidos.
+
+
+-- 12.18. O somatório do valor do pedido agrupado por cliente.
+
+
+-- 12.19. O somatório do valor do pedido agrupado por vendedor.
+
+
+-- 12.20. O somatório do valor do pedido agrupado por transportadora.
+
+
+-- 12.21. O somatório do valor do pedido agrupado pela data.
+
+
+-- 12.22. O somatório do valor do pedido agrupado por cliente, vendedor e transportadora.
+
+
+-- 12.23. O somatório do valor do pedido que esteja entre 01/04/2008 e 10/12/2009 e que seja maior que R$ 200,00.
+
+
+-- 12.24. A média do valor do pedido do vendedor André.
+
+
+-- 12.25. A média do valor do pedido da cliente Jéssica.
+
+
+-- 12.26. A quantidade de pedidos transportados pela transportadora BS. Transportes.
+
+
+-- 12.27. A quantidade de pedidos agrupados por vendedor.
+
+
+-- 12.28. A quantidade de pedidos agrupados por cliente.
+
+
+-- 12.29. A quantidade de pedidos entre 15/04/2008 e 25/04/2008.
+
+
+-- 12.30. A quantidade de pedidos que o valor seja maior que R$ 1.000,00.
+
+
+-- 12.31. A quantidade de microcomputadores vendida.
+
+
+-- 12.32. A quantidade de produtos vendida, agrupada por produto.
+
+
+-- 12.33. O somatório do valor dos produtos dos pedidos, agrupado por pedido.
+
+
+-- 12.34. A quantidade de produtos agrupados por pedido.
+
+
+-- 12.35. O somatório do valor total de todos os produtos do pedido.
+
+
+-- 12.36. A média dos produtos do pedido 06.
+
+
+-- 12.37. O valor do maior produto do pedido.
+
+
+-- 12.38. O valor do menor produto do pedido.
+
+
+-- 12.39. O somatório da quantidade de produtos por pedido.
+
+
+-- 12.40. O somatório da quantidade de todos os produtos do pedido.
